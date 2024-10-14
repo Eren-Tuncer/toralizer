60d40635125ee20fceef931302f14752cc6bf480
eren_tuncer <erentuncer53@gmail.com>
2024-10-14 22:31:04 +0300

    first_commit

--- toralize.c
+++ toralize.c
@@ -2,6 +2,7 @@
 
 #include "toralize.h"
 
+
 /*
 ./toralize <IP> <PORT> 
 */
@@ -9,7 +10,15 @@
 
 int main(int argc, char *argv[]){
 
+    char *hostname;
+    int port;
 
+    if (argc<3){
+        fprintf(stderr, "Usage: %s <host> <port>\n",
+        argv[0]);
+        return(-1);
+    }
 
-    
+    host = argv[1];
+    port = atoi(argv[2]);
 }
